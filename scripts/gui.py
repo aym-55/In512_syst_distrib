8faa327c221dac1babeb2e1e2539bae09e40157b
Aymeric <aymeric.legrand7777@hotmail.com>
2024-11-18 10:00:32 +0100

    size of cells

--- scripts/gui.py
+++ scripts/gui.py
@@ -11,7 +11,7 @@ img_folder = os.path.join(os.path.dirname(os.path.dirname(os.path.abspath(__file
 
 
 class GUI:
-    def __init__(self, game, fps=10, cell_size=40):
+    def __init__(self, game, fps=10, cell_size=30):
         self.game = game
         self.w, self.h = self.game.map_w, self.game.map_h
         self.fps = fps
